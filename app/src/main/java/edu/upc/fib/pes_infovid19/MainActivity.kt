c40a454852d59b221d2b23b452542407bcec10a9
Laia42 <laia.igelmo@upc.edu>
2020-10-16 18:16:53 +0200

    Delete autogenerated MainFragment.kt and main_fragment.xml since we are not going to use fragments.

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
-import edu.upc.fib.pes_infovid19.ui.main.MainFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
         buttonConsultarInfo.setOnClickListener {
             val intent = Intent(this, HealthMenuActivity::class.java)
             startActivity(intent)

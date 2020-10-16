86a7ef344a358f4541463b0c4e3453f78b01d439
Laia42 <laia.igelmo@upc.edu>
2020-10-16 20:29:52 +0200

    Add myth activity with the respective navegability.

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/MythsActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/MythsActivity.kt
@@ -1,22 +1,17 @@
 package edu.upc.fib.pes_infovid19.ui.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_health_menu.*
 
-class HealthMenuActivity : AppCompatActivity() {
+class MythsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_health_menu)
+        setContentView(R.layout.activity_myths)
+
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        mitesCovid.setOnClickListener {
-            val intent = Intent(this, MythsActivity::class.java)
-            startActivity(intent)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {

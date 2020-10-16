c98ee7cc230dff5ec9e1160bcc7bcd56edf23bac
Laia42 <laia.igelmo@upc.edu>
2020-10-16 18:13:18 +0200

    Add back navigation from health menu to main menu.

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -1,8 +1,11 @@
 package edu.upc.fib.pes_infovid19
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
 import edu.upc.fib.pes_infovid19.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+        buttonConsultarInfo.setOnClickListener {
+            val intent = Intent(this, HealthMenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }

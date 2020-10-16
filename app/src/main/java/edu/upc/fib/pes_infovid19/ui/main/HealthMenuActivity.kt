be9386e48fd033d6b05fe2afe11f7599e05959b4
Laia42 <laia.igelmo@upc.edu>
2020-10-16 16:14:18 +0200

    Add health menu buttons and title on activity.

---
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HealthMenuActivity.kt
@@ -0,0 +1,12 @@
+package edu.upc.fib.pes_infovid19.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.upc.fib.pes_infovid19.R
+
+class HealthMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health_menu)
+    }
+}

7febf7aa7f2702cfe7e2accb8f645396ef68e203
Anvii8 <albert.andres.vidueira@estudiantat.upc.edu>
2020-10-17 17:11:02 +0200

    add test questions add switches add buttons add toolbar

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -1,12 +1,9 @@
 package edu.upc.fib.pes_infovid19
 
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import edu.upc.fib.pes_infovid19.ui.main.MainFragment
-import kotlinx.android.synthetic.main.activity_vulnerable_test.*
-
-
 import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -18,16 +15,15 @@ class MainActivity : AppCompatActivity() {
         buttonConsultarInfo.setOnClickListener {
             val intent = Intent(this, HealthMenuActivity::class.java)
             startActivity(intent)
+        }
 
-            buttonObrirTestVulnerable.setOnClickListener {
-                startActivity(
-                    Intent(
-                        this,
-                        VulnerableTestActivity::class.java
-                    )
+        buttonObrirTestVulnerable.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    VulnerableTestActivity::class.java
                 )
-            }
-
+            )
         }
     }
 }

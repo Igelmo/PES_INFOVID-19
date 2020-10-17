d4da2ab2427bfb08ed92d7c0dc3ebab392d15bd2
Anvii8 <albert.andres.vidueira@estudiantat.upc.edu>
2020-10-17 12:23:57 +0200

    working progress activity vulnerable

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -1,18 +1,31 @@
 package edu.upc.fib.pes_infovid19
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.ui.main.MainFragment
+import kotlinx.android.synthetic.main.activity_vulnerable_test.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        buttonObrirTestVulnerable.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    VulnerableTestActivity::class.java
+                )
+            )
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
---
+++ app/src/main/java/edu/upc/fib/pes_infovid19/VulnerableTestActivity.kt
@@ -0,0 +1,12 @@
+package edu.upc.fib.pes_infovid19
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VulnerableTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vulnerable_test)
+
+    }
+}

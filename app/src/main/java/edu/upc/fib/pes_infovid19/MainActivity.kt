5a960c9e5f7400daf8a206d632190a670bc8b834
Laia42 <laia.igelmo@upc.edu>
2020-10-20 00:19:45 +0200

    -Change name of Centre Hospitalari activity (.kt and .xml) to Hospital Center activity -Create Infection Probability Test activity (.kt and .xml) -Add toolbar to infection probability test activity -Add navigation between main activity and infection probability test activity

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -4,8 +4,9 @@ package edu.upc.fib.pes_infovid19
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import edu.upc.fib.pes_infovid19.ui.main.CentreHospitalariActivity
 import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
+import edu.upc.fib.pes_infovid19.ui.main.HospitalCenterActivity
+import edu.upc.fib.pes_infovid19.ui.main.InfectionProbabilityTestActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonCentreHospitalari.setOnClickListener {
-            val intent = Intent(this, CentreHospitalariActivity::class.java)
+            val intent = Intent(this, HospitalCenterActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonTestContagi.setOnClickListener {
+            val intent = Intent(this, InfectionProbabilityTestActivity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
@@ -3,12 +3,12 @@ package edu.upc.fib.pes_infovid19.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.R
-import kotlinx.android.synthetic.main.activity_centre_hospitalari.*
+import kotlinx.android.synthetic.main.activity_hospital_center.*
 
-class CentreHospitalariActivity : AppCompatActivity() {
+class HospitalCenterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_centre_hospitalari)
+        setContentView(R.layout.activity_hospital_center)
         setSupportActionBar(toolbarCentreHospitalari)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }

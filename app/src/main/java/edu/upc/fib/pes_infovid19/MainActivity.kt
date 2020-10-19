a731e3d4d00a1d8bc96cc32077e90afcf7d3bf93
Laia42 <laia.igelmo@upc.edu>
2020-10-20 00:09:19 +0200

    -Create Centre Hospitalari activity (.kt and .xml) -Add toolbar to Centre Hospitalari activity -Add navigation between main activity and centre hospitalari activity

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -4,6 +4,7 @@ package edu.upc.fib.pes_infovid19
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import edu.upc.fib.pes_infovid19.ui.main.CentreHospitalariActivity
 import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        buttonCentreHospitalari.setOnClickListener {
+            val intent = Intent(this, CentreHospitalariActivity::class.java)
+            startActivity(intent)
+        }
+
         buttonObrirTestVulnerable.setOnClickListener {
             startActivity(
                 Intent(
@@ -25,6 +31,7 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+
         fab.setOnClickListener {
         }
     }

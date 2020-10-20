bad056b59cf7f87086738671ee6afe3778bab60d
carlesll <carlesllongueras@hotmail.com>
2020-10-20 15:17:06 +0200

    - Open formulari - Back to main menu - Insert values - Accept values - Text complete - You can change the text - Pull of develop

--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -4,7 +4,7 @@ package edu.upc.fib.pes_infovid19
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
+import edu.upc.fib.pes_infovid19.ui.main.*
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        buttonFerErte.setOnClickListener {
+        buttonERTE.setOnClickListener {
             val intent = Intent(this, ErteActivity::class.java)
             startActivity(intent)
         }

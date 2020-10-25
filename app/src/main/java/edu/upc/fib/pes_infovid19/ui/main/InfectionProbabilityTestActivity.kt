77beed16f0228d4abb4efa0352e266441c716e42
sergi <sergi.bosquet@estudiantat.upc.edu>
2020-10-25 12:25:45 +0100

    - Create new branch resultat-test-vulnerable. - Create new activity ResultVulnerableTestActivity. - Create a drawable circle. - Create interface from activity ResultVulnerableTestActivity. - Generate Health, Economic and Social results. - Implement the navigavilities of the interface. - Show the results in a circular graphic in the interface. - Modify the navigabilities to go back to the main menu in ChatActivity and InfectionProbabilityTestActivity.

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/InfectionProbabilityTestActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/InfectionProbabilityTestActivity.kt
@@ -1,7 +1,9 @@
 package edu.upc.fib.pes_infovid19.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import edu.upc.fib.pes_infovid19.MainActivity
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_infection_probability_test.*
 
@@ -9,13 +11,9 @@ class InfectionProbabilityTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infection_probability_test)
-        setSupportActionBar(toolbarInfectionTest)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarInfectionTest.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
-    }
-
 }

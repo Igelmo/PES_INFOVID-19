1d91f30f45e7182e1d0ad91ef7b51d33ec789d60
sergi <sergi.bosquet@estudiantat.upc.edu>
2020-11-07 11:22:36 +0100

    - Create function generate result of infection probability test.

--- app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
@@ -2,6 +2,7 @@ package edu.upc.fib.pes_infovid19
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_infection_probability_test.*
 
@@ -13,5 +14,23 @@ class InfectionProbabilityTestActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun GenerateResult(View: View) {
+        var percent = 0.0
+        if (switch1.isChecked) percent += 7.5
+        if (switch2.isChecked) percent += 6
+        if (switch3.isChecked) percent += 7.5
+        if (switch4.isChecked) percent += 15
+        if (switch5.isChecked) percent += 6.5
+        if (switch6.isChecked) percent += 7
+        if (switch7.isChecked) percent += 15
+        if (switch8.isChecked) percent += 15
+        if (switch9.isChecked) percent += 7.5
+        if (switch10.isChecked) percent += 7
+        if (switch11.isChecked) percent += 6
+        val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
+        intent.putExtra("percent", percent)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ResultInfectionProbabilityTestActivity.kt
@@ -0,0 +1,11 @@
+package edu.upc.fib.pes_infovid19
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultInfectionProbabilityTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_infection_probability_test)
+    }
+}

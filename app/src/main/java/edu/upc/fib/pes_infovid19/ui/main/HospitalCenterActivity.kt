7f70852435bfd417d17b3075a434cdc863e4017f
carlesll <carlesllongueras@hotmail.com>
2020-11-09 19:56:53 +0100

    - nada

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
@@ -3,15 +3,42 @@ package edu.upc.fib.pes_infovid19.ui.main
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_hospital_center.*
 
+
 class HospitalCenterActivity : AppCompatActivity() {
+    lateinit var googleMap: GoogleMap
+    lateinit var mapview: MapView
+
+    override fun onResume() {
+        super.onResume()
+        mapview.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapview.onDestroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapview.onPause()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hospital_center)
         setSupportActionBar(toolbarCentreHospitalari)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mapview = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        googleMap = mapView.getMap()
+        googleMap = setMapType(GoogleMap.MAP_TYPE_NORMAL)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -23,4 +50,7 @@ class HospitalCenterActivity : AppCompatActivity() {
 
     }
 
+
 }
+
+

4dec04d6ec08a147b55d4753b441816f4c40c568
carlesll <carlesllongueras@hotmail.com>
2020-11-10 16:33:28 +0100

    - nada

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/HospitalCenterActivity.kt
@@ -5,41 +5,57 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_hospital_center.*
 
 
-class HospitalCenterActivity : AppCompatActivity() {
-    lateinit var googleMap: GoogleMap
-    lateinit var mapview: MapView
+class HospitalCenterActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    override fun onResume() {
-        super.onResume()
-        mapview.onResume()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapview.onDestroy()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapview.onPause()
-    }
-
+    var mMapView: MapView = TODO()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hospital_center)
         setSupportActionBar(toolbarCentreHospitalari)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        mapview = findViewById(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        googleMap = mapView.getMap()
-        googleMap = setMapType(GoogleMap.MAP_TYPE_NORMAL)
+        mMapView = findViewById<MapView>(R.id.mapView)
+        mMapView.getMapAsync(this)
 
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+    }
+
+    override fun onPause() {
+        mMapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mMapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mMapView.onSaveInstanceState(outState)
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()

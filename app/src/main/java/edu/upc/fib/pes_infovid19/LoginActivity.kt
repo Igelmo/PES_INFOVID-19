89c7161a121040b3fe335cc2139f2250dd60680f
Anvii8 <albert.andres.vidueira@estudiantat.upc.edu>
2020-11-14 13:47:48 +0100

    create login activity create interface login activity create provisional login button in Main activity create comunication with firebase create login with email and password create login with google create navigabilities

---
+++ app/src/main/java/edu/upc/fib/pes_infovid19/LoginActivity.kt
@@ -0,0 +1,141 @@
+package edu.upc.fib.pes_infovid19
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+enum class ProviderType {
+    GOOGLE
+}
+
+class LoginActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Analytics Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+        signupbutton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && PasswordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), PasswordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        loginbutton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && PasswordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(), PasswordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        googlebutton.setOnClickListener {
+
+            // Configuración
+
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
+
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
+    }
+}
+/*
+        !!!!!! NO BORRAR !!!!!!
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if(email != null && provider != null){
+
+        }
+    }
+/*private fun Setup(email: String, provider: String){
+    val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+    prefs.clear()
+    prefs.apply()
+}*/
+
+}*/
--- app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/MainActivity.kt
@@ -4,7 +4,10 @@ package edu.upc.fib.pes_infovid19
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import edu.upc.fib.pes_infovid19.ui.main.*
+import edu.upc.fib.pes_infovid19.ui.main.ChatActivity
+import edu.upc.fib.pes_infovid19.ui.main.HealthMenuActivity
+import edu.upc.fib.pes_infovid19.ui.main.HospitalCenterActivity
+import edu.upc.fib.pes_infovid19.ui.main.UserProfileActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -39,6 +42,11 @@ class MainActivity : AppCompatActivity() {
 
         buttonERTE.setOnClickListener {
             val intent = Intent(this, ErteActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonlogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 

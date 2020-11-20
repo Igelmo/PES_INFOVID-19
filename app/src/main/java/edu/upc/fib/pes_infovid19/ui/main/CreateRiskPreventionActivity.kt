29911e5575f469935da1b16bd680154efa2caa5e
Laia42 <laia.igelmo@upc.edu>
2020-11-20 20:23:58 +0100

    - Remove risk prevention implemented

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/CreateRiskPreventionActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/CreateRiskPreventionActivity.kt
@@ -2,17 +2,20 @@ package edu.upc.fib.pes_infovid19.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_create_risk_prevention.*
 
 class CreateRiskPreventionActivity : AppCompatActivity() {
+    private val viewModel: RiskPreventionViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_risk_prevention)
         setSupportActionBar(toolbarCreateRiskPrevention)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         createNewRiskPreventionButton.setOnClickListener {
+            constructNewRiskPrevention()
             onSupportNavigateUp()
         }
         fabCreateNewPrevention.setOnClickListener {
@@ -21,8 +24,12 @@ class CreateRiskPreventionActivity : AppCompatActivity() {
         }
     }
 
-    fun constructNewTestType() {
-
+    fun constructNewRiskPrevention() {
+        val riskPrevention = RiskPrevention()
+        riskPrevention.title = titleTextRiskPrevention.text.toString()
+        riskPrevention.date = createDateRiskPrevention.text.toString()
+        riskPrevention.source = createSourceRiskPrevention.text.toString()
+        viewModel.addRiskPrevention(riskPrevention)
     }
 
     override fun onSupportNavigateUp(): Boolean {

cbdbb130ee8f8037bfa130f1b4714e3675266b4e
Laia42 <laia.igelmo@upc.edu>
2020-11-29 17:27:55 +0100

    - Implement create functionality of prevention

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/CreatePreventionActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/CreatePreventionActivity.kt
@@ -1,23 +1,31 @@
 package edu.upc.fib.pes_infovid19.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_create_prevention.*
 
 class CreatePreventionActivity : AppCompatActivity() {
+    private val viewModel: RiskPreventionViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_prevention)
         setSupportActionBar(toolbarCreatePrevention)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         createNewPreventionButton.setOnClickListener {
+            constructNewPrevention()
             onSupportNavigateUp()
         }
     }
 
-    fun constructNewTestType() {
-
+    fun constructNewPrevention() {
+        val prevention = Prevention()
+        prevention.title = titleTextPrevention.text.toString()
+        prevention.text = textPrevention.text.toString()
+        prevention.image = urlimage.text.toString()
+        viewModel.addPrevention(prevention)
     }
 
     override fun onSupportNavigateUp(): Boolean {

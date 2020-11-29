e0c2a8ba3920ceabdf51aa150e4143a117724b7d
Laia42 <laia.igelmo@upc.edu>
2020-11-29 17:22:36 +0100

    - Implement update functionality of prevention

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/EditPreventionActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/EditPreventionActivity.kt
@@ -1,6 +1,7 @@
 package edu.upc.fib.pes_infovid19.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_edit_prevention.*
@@ -8,6 +9,8 @@ import kotlinx.android.synthetic.main.activity_edit_prevention.*
 const val PREVENTION_EXTRA = "PREVENTION_EXTRA"
 
 class EditPreventionActivity : AppCompatActivity() {
+    private val viewModel: RiskPreventionViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_prevention)
@@ -16,8 +19,17 @@ class EditPreventionActivity : AppCompatActivity() {
         val prevention = intent.getSerializableExtra(PREVENTION_EXTRA) as Prevention
         setInfo(prevention)
         editPreventionButton.setOnClickListener {
+            saveChanges(prevention.id)
             onSupportNavigateUp()
         }
+    }
+
+    private fun saveChanges(id: String?) {
+        val title = titleTextEditPrevention.text.toString()
+        val text = textEditPrevention.text.toString()
+        val image = imageEditPrevention.text.toString()
+        val prevention = Prevention(id as String, title, text, image)
+        viewModel.modifyPrevention(id, prevention)
     }
 
     private fun setInfo(prevention: Prevention) {

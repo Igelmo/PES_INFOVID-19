d2c7968728ba377c66e78a576d4f3df837aa4316
Laia42 <laia.igelmo@upc.edu>
2020-12-08 22:46:37 +0100

    REFACTOR: - Use the data repository with the viewmodel to add the questions on the recycler view of infection probability test. - Create an adapter to fill the recyclerview.

--- app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
@@ -3,10 +3,14 @@ package edu.upc.fib.pes_infovid19
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
+import edu.upc.fib.pes_infovid19.ui.main.InfectionProbabilityTestAdapter
 import kotlinx.android.synthetic.main.activity_infection_probability_test.*
 
 class InfectionProbabilityTestActivity : AppCompatActivity() {
+    private val viewModel: InfectionProbabilityTestViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infection_probability_test)
@@ -17,9 +21,16 @@ class InfectionProbabilityTestActivity : AppCompatActivity() {
             val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
             startActivity(intent)
         }
+
+        val adapter = InfectionProbabilityTestAdapter(false)
+        recyclerViewInfectionTest.adapter = adapter
+
+        viewModel.questionsProbabilityTestLiveData.observe(this) { questionsSnapshot ->
+            adapter.updateQuestions(questionsSnapshot)
+        }
     }
 
-    fun generateInfectionResult(View: View) {
+    private fun generateInfectionResult(View: View) {
         var percent = 0.0
         val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
         intent.putExtra("percent", percent)
--- app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestViewModel.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestViewModel.kt
@@ -8,5 +8,10 @@ import edu.upc.fib.pes_infovid19.ui.main.QuestionProbabilityTest
 
 class InfectionProbabilityTestViewModel : ViewModel() {
     private val repository: InfectionProbabilityTestRepository = InfectionProbabilityTestFirebaseRepository()
+
+    fun deleteQuestionInfectionProbabilityTest(id: String) = repository.removeQuestionInfectionProbabilityTest(id)
+    fun modifyQuestionInfectionProbabilityTest(id: String, question: QuestionProbabilityTest) = repository.modifyQuestionInfectionProbabilityTest(id, question)
+    fun addQuestionInfectionProbabilityTest(question: QuestionProbabilityTest) = repository.createQuestionInfectionProbabilityTest(question)
+
     val questionsProbabilityTestLiveData: LiveData<List<QuestionProbabilityTest>> = repository.getQuestionsInfectionProbabilityTest()
 }

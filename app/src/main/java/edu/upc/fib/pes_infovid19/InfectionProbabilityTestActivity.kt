6229ded1c7ce4393c968794bff9d106d3bb886b3
Laia42 <laia.igelmo@upc.edu>
2020-12-08 20:05:31 +0100

    REFACTOR: - Add recycler view of questions. - Change the switch item to a material switch in order to unify switch and text. - Change navegability to on back pressed to avoid possible problems.

--- app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
@@ -10,27 +10,24 @@ class InfectionProbabilityTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infection_probability_test)
-        toolbarInfectionTest.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        setSupportActionBar(toolbarInfectionTest)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        buttonCheckProbability.setOnClickListener {
+            val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
             startActivity(intent)
         }
     }
 
     fun generateInfectionResult(View: View) {
         var percent = 0.0
-        if (switch1.isChecked) percent += 7.5
-        if (switch2.isChecked) percent += 6
-        if (switch3.isChecked) percent += 7.5
-        if (switch4.isChecked) percent += 15
-        if (switch5.isChecked) percent += 6.5
-        if (switch6.isChecked) percent += 7
-        if (switch7.isChecked) percent += 15
-        if (switch8.isChecked) percent += 15
-        if (switch9.isChecked) percent += 7.5
-        if (switch10.isChecked) percent += 7
-        if (switch11.isChecked) percent += 6
         val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
         intent.putExtra("percent", percent)
         startActivity(intent)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }

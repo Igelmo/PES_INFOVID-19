77a2e5d7686ff4ae38abd6527d2cacd8826b9385
Laia42 <laia.igelmo@upc.edu>
2020-12-10 17:41:12 +0100

    - Create Interface to manage questions of probability test. - Create new adapter and item for the recycler view of managable questions.

--- app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/InfectionProbabilityTestActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEachIndexed
 import androidx.lifecycle.observe
 import edu.upc.fib.pes_infovid19.ui.main.InfectionProbabilityTestAdapter
+import edu.upc.fib.pes_infovid19.ui.main.ManageQuestionsProbabilityTestActivity
 import edu.upc.fib.pes_infovid19.ui.main.QuestionProbabilityTest
 import kotlinx.android.synthetic.main.activity_infection_probability_test.*
 import kotlinx.android.synthetic.main.question_test_item.view.*
@@ -26,6 +27,11 @@ class InfectionProbabilityTestActivity : AppCompatActivity() {
             val intent = Intent(this, ResultInfectionProbabilityTestActivity::class.java)
             startActivity(intent)
             generateInfectionResult()
+        }
+
+        buttonManageProbabilityQuestions.setOnClickListener {
+            val intent = Intent(this, ManageQuestionsProbabilityTestActivity::class.java)
+            startActivity(intent)
         }
 
         recyclerViewInfectionTest.adapter = adapter
---
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/ManageQuestionsProbabilityTestActivity.kt
@@ -0,0 +1,32 @@
+package edu.upc.fib.pes_infovid19.ui.main
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
+import edu.upc.fib.pes_infovid19.InfectionProbabilityTestViewModel
+import edu.upc.fib.pes_infovid19.R
+import kotlinx.android.synthetic.main.activity_manage_questions_probability_test.*
+
+class ManageQuestionsProbabilityTestActivity : AppCompatActivity() {
+    private val viewModel: InfectionProbabilityTestViewModel by viewModels()
+    private val adapter = ManageInfectionProbabilityTestAdapter()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_questions_probability_test)
+        setSupportActionBar(toolbarManageQuestionsProbabilityTest)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        recyclerViewManageInfectionTest.adapter = adapter
+
+        viewModel.questionsProbabilityTestLiveData.observe(this) { questionsSnapshot ->
+            adapter.updateQuestions(questionsSnapshot)
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}

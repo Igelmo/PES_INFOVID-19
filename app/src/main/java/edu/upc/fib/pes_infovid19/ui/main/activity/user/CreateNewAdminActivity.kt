e1763991ab7059f607bfc20f8bf386f502921434
sergibosquet <sergi.bosquet@estudiantat.upc.edu>
2020-12-20 10:22:30 +0100

    - Change google signin button to login. - Create admin main activity. - Create create new admin activity. - Create admin main activity interface. - Create create new admin activity interface. - Implement navigabilities to manage activities.

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/user/CreateNewAdminActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/user/CreateNewAdminActivity.kt
@@ -6,21 +6,14 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import edu.upc.fib.pes_infovid19.R
 import edu.upc.fib.pes_infovid19.domain.structures.User
-import edu.upc.fib.pes_infovid19.ui.main.activity.MainActivity
 import kotlinx.android.synthetic.main.activity_create_new_admin.*
 
 private var newUser = User()
 
 class CreateNewAdminActivity : AppCompatActivity() {
-    val user = FirebaseAuth.getInstance().currentUser
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_admin)
@@ -43,38 +36,6 @@ class CreateNewAdminActivity : AppCompatActivity() {
         }
     }
 
-    private fun lastSession(user: FirebaseUser?) {
-        if (user != null) {
-            val mDatabase = FirebaseDatabase.getInstance().reference.child("User").orderByChild("email").equalTo(user.email)
-            mDatabase.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    var password: String = ""
-                    for (snapshot in dataSnapshot.children) {
-                        password = snapshot.child("password").getValue(String::class.java)!!
-                    }
-                    newfunction(password)
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    println("The read failed: " + databaseError.code)
-                }
-            })
-        }
-    }
-
-    private fun newfunction(password: String) {
-        val db = FirebaseAuth.getInstance()
-        db.signInWithEmailAndPassword(user?.email.toString(), password).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            } else {
-                showAlert()
-            }
-        }
-    }
-
-
     fun fillAdmin(): User {
         var et = findViewById<EditText>(R.id.emailEditTextAdmin)
         var email = et.text.toString()
@@ -96,7 +57,6 @@ class CreateNewAdminActivity : AppCompatActivity() {
         if (id != null) {
             database.child("User").child(id).setValue(newUser).addOnCompleteListener {
                 if (it.isSuccessful) {
-                    lastSession(user)
                     val intent = Intent(this, AdminMainActivity::class.java)
                     startActivity(intent)
                 } else {

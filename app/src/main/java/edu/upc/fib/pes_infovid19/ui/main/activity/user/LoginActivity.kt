75cdfe9569163fe2836b16b547a10b7fd84f347b
Laia42 <laia.igelmo@upc.edu>
2020-12-29 00:12:19 +0100

    - add splash screen

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/user/LoginActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/user/LoginActivity.kt
@@ -26,7 +26,7 @@ class LoginActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 

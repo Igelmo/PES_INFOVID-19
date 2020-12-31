a9756521a376c05f85c0733c3f6a0cfacdcd3b4e
Laia42 <laia.igelmo@upc.edu>
2020-12-31 18:57:35 +0100

    - Add interface to hospital center - Add apy key for google maps api - Add geocoder

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/health/hospitalcenter/HospitalCenterActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/health/hospitalcenter/HospitalCenterActivity.kt
@@ -1,16 +1,97 @@
 package edu.upc.fib.pes_infovid19.ui.main.activity.health.hospitalcenter
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_hospital_center.*
 
-class HospitalCenterActivity : AppCompatActivity() {
+
+class HospitalCenterActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hospital_center)
         setSupportActionBar(toolbarCentreHospitalari)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode
+            )
+            return
+        }
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                Toast.makeText(
+                    applicationContext, currentLocation.latitude.toString() + "" +
+                            currentLocation.longitude, Toast.LENGTH_SHORT
+                ).show()
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.map) as
+                        SupportMapFragment?)!!
+                supportMapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val geocoder = Geocoder(this)
+        val list = geocoder.getFromLocationName("Hospital", 5, currentLocation.latitude - 30f, currentLocation.longitude - 30f, currentLocation.latitude + 30f, currentLocation.longitude + 30f)
+        val latLng = LatLng(list[0].latitude, list[0].longitude)
+        val localizationHospital: TextView = findViewById(R.id.hospitalLocation)
+        val nameHospital: TextView = findViewById(R.id.hospitalName)
+        localizationHospital.text = list[0].getAddressLine(0)
+        nameHospital.text = list[0].phone
+
+        val markerOptions = MarkerOptions().position(latLng).title("Hospital més proper!")
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        googleMap?.addMarker(markerOptions)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {

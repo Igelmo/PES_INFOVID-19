bbc1bbdc36624203cbba36283f2123128f7948d2
Laia42 <laia.igelmo@upc.edu>
2021-01-06 19:42:38 +0100

    - Fixed issues with the last API - Change the API with Place API

--- app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/health/hospitalcenter/HospitalCenterActivity.kt
+++ app/src/main/java/edu/upc/fib/pes_infovid19/ui/main/activity/health/hospitalcenter/HospitalCenterActivity.kt
@@ -16,10 +16,15 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.maps.GeoApiContext
-import com.google.maps.PlaceDetailsRequest
-import com.google.maps.PlacesApi
-import com.google.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
+import com.google.android.libraries.places.api.net.PlacesClient
 import edu.upc.fib.pes_infovid19.R
 import kotlinx.android.synthetic.main.activity_hospital_center.*
 import com.google.android.gms.maps.model.LatLng as GmsLatLng
@@ -86,39 +91,54 @@ class HospitalCenterActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun nearbySearch(): List<PlaceDetails> {
+    private fun nearbySearch(listener: (Place) -> Unit) {
+        Places.initialize(applicationContext, "AIzaSyBrZKY5DQjUK8X6TjrOUKjtkZmrvikYUdk")
+        val placesClient = Places.createClient(this)
+        val latLng = GmsLatLng(currentLocation.latitude, currentLocation.longitude)
+        val bounds = RectangularBounds.newInstance(
+            GmsLatLng(latLng.latitude - 0.01, latLng.longitude - 0.01),
+            GmsLatLng(latLng.latitude + 0.01, latLng.longitude + 0.01)
+        )
+        val request = FindAutocompletePredictionsRequest.builder()
+            .setOrigin(latLng)
+            .setLocationBias(bounds)
+            .setTypeFilter(TypeFilter.ESTABLISHMENT)
+            .setQuery("hospital")
+            .build()
+        placesClient.findAutocompletePredictions(request)
+            .addOnSuccessListener { response: FindAutocompletePredictionsResponse ->
+                response.autocompletePredictions
+                    .sortedBy { it.distanceMeters }
+                    .firstOrNull()
+                    ?.let { placesClient.getPlaceDetails(it, listener) }
+            }
+    }
 
-        val searchContext = GeoApiContext.Builder().apiKey("AIzaSyBrZKY5DQjUK8X6TjrOUKjtkZmrvikYUdk").build()
-        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
-
-        val request = PlacesApi.nearbySearchQuery(searchContext, latLng)
-            .rankby(RankBy.DISTANCE)
-            .type(PlaceType.HOSPITAL)
-            .openNow(true)
-            .keyword("hospital public a prop meu")
-            .await()
-        return request.results.map {
-            PlacesApi.placeDetails(searchContext, it.placeId).fields(
-                PlaceDetailsRequest.FieldMask.INTERNATIONAL_PHONE_NUMBER,
-                PlaceDetailsRequest.FieldMask.NAME,
-                PlaceDetailsRequest.FieldMask.VICINITY,
-                PlaceDetailsRequest.FieldMask.GEOMETRY_LOCATION_LAT,
-                PlaceDetailsRequest.FieldMask.GEOMETRY_LOCATION_LNG
-            ).await()
+    private fun PlacesClient.getPlaceDetails(prediction: AutocompletePrediction, listener: (Place) -> Unit) {
+        val placeFields = listOf(Place.Field.LAT_LNG, Place.Field.ADDRESS, Place.Field.NAME, Place.Field.PHONE_NUMBER)
+        val request = FetchPlaceRequest.newInstance(prediction.placeId, placeFields)
+        fetchPlace(request).addOnSuccessListener {
+            listener(it.place)
         }
     }
 
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        val placesSearchResults = nearbySearch()
-        val locationHospital = GmsLatLng(placesSearchResults[0].geometry.location.lat, placesSearchResults[0].geometry.location.lng)
-
-        googleMap?.addMarker(MarkerOptions().position(locationHospital).title("Hospital més proper"))
-        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(GmsLatLng(placesSearchResults[0].geometry.location.lat, placesSearchResults[0].geometry.location.lng), 17.0f))
-
-        hospitalLocation.text = placesSearchResults[0].vicinity
-        hospitalNameText.text = placesSearchResults[0].name
-        hospitalPhone = placesSearchResults[0].internationalPhoneNumber
+        nearbySearch { place ->
+            place.latLng?.let { locationHospital ->
+                googleMap?.addMarker(MarkerOptions().position(locationHospital).title("Hospital més proper"))
+                googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(locationHospital, 17.0f))
+            }
+            place.name?.let {
+                hospitalNameText.text = it
+            }
+            place.address?.let {
+                hospitalLocation.text = it
+            }
+            place.phoneNumber?.let {
+                hospitalPhone = it
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
